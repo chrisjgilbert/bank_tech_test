e1e6398f281ee451ff6bb503d50be04db7d765f6
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 12:53:48 +0000

    red: deposite adds money to the balance

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -2,9 +2,19 @@ describe("account", function() {
 
   var account;
 
-  it("has a starting balance of 0", function() {
+  beforeEach(function() {
     account = new Account()
+  })
+
+  it("has a starting balance of 0", function() {
     expect(account.balance).toBe(0)
+  })
+
+  describe("deposit", function() {
+    it("adds money to the balance", function() {
+      account.deposit(5)
+      expect(account.balance).toEqual(5)
+    })
   })
 
 })
