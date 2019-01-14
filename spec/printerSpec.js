18ffbd027cdc2c2fd5276b685bfe7b9fd51bca0c
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 17:22:59 +0000

    red: printer stores statement header

---
+++ spec/printerSpec.js
@@ -0,0 +1,14 @@
+"use strict";
+describe("printer", function() {
+
+  var printer;
+
+  beforeEach(function() {
+    printer = new Printer
+  })
+
+  it("stores the statement header", function() {
+    expect(printer.showHeader()).toEqual(["DATE", "DEBIT", "CREDIT", "BALANCE"])
+  })
+
+})
