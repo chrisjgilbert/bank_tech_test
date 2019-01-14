f985bb271104a9275b86bbf4f33002474e91ed7a
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 20:18:33 +0000

    red: withdraw adds amount, date, empty string and updated balance to history

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -2,8 +2,14 @@
 describe("account", function() {
 
   var account;
+  var dateGenerator;
+  var injectedAccount;
 
   beforeEach(function() {
+    dateGenerator = jasmine.createSpyObj('dateGenerator', {
+        'getFullDate': '01/01/1998'
+    });
+    injectedAccount = new Account(dateGenerator)
     account = new Account()
   })
 
@@ -16,16 +22,6 @@ describe("account", function() {
   })
 
   describe("deposit", function() {
-
-    var dateGenerator;
-    var injectedAccount;
-
-    beforeEach(function() {
-      dateGenerator = jasmine.createSpyObj('dateGenerator', {
-          'getFullDate': '01/01/1998'
-      });
-      injectedAccount = new Account(dateGenerator)
-    })
 
     it("adds money to the balance", function() {
       account.deposit(5)
@@ -51,6 +47,11 @@ describe("account", function() {
     it("deducts money from the balance", function() {
       account.withdraw(5)
       expect(account.showBalance()).toEqual(-5)
+    })
+
+    it("adds withdraw amount, date, empty string and balance to history", function() {
+      injectedAccount.withdraw(100)
+      expect(injectedAccount.getHistory()).toEqual([["01/01/1998", 100, "", -100]])
     })
   })
 
