503bb7737b7cff9309cf7d17f7b8c27b4d0c18d5
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 13:03:01 +0000

    tests updated to use showBalance function

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -7,20 +7,20 @@ describe("account", function() {
   })
 
   it("has a starting balance of 0", function() {
-    expect(account.balance).toEqual(0)
+    expect(account.showBalance()).toEqual(0)
   })
 
   describe("deposit", function() {
     it("adds money to the balance", function() {
       account.deposit(5)
-      expect(account.balance).toEqual(5)
+      expect(account.showBalance()).toEqual(5)
     })
   })
 
   describe("withdraw", function() {
     it("deducts money from the balance", function() {
       account.withdraw(5)
-      expect(account.balance).toEqual(-5)
+      expect(account.showBalance()).toEqual(-5)
     })
   })
 
