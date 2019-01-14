b464f9ccf2cc3af1765350fa270a099ac4086c0b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 13:32:08 +0000

    green: log is empty by default

--- src/accountHistory.js
+++ src/accountHistory.js
@@ -1,1 +1,14 @@
-function AccountHistory() {}
+(function(exports) {
+
+  function AccountHistory() {
+    this._log = []
+  }
+
+  AccountHistory.prototype = {
+    showLog: function() {
+      return this._log
+    }
+  }
+
+  exports.AccountHistory = AccountHistory
+})(this)
