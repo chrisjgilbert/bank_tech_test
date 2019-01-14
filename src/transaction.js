a8e4e6ce2bb6ae78d320e3313007af91056046a0
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 14:01:46 +0000

    green: transaction has an amount

--- src/transaction.js
+++ src/transaction.js
@@ -1,12 +1,17 @@
 (function(exports) {
 
-  function Transaction(type) {
+  function Transaction(type, amount) {
     this._type = type
+    this._amount = amount
   }
 
   Transaction.prototype = {
     showType: function() {
       return this._type
+    },
+
+    showAmount: function() {
+      return this._amount
     }
   }
 
