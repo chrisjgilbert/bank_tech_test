651c45ad945cd54bd71c3155dd59c49d8fc11e6b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 17:35:48 +0000

    red: statement pretty prints

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -11,4 +11,11 @@ describe("printer", function() {
     expect(statement.showHeader()).toEqual(["DATE", "DEBIT", "CREDIT", "BALANCE"])
   })
 
+  it("pretty prints out the statement", function() {
+    var log = [["14/01/2019", "0", "10", "0"]]
+    expect(statement.print(log)).toEqual(
+      "01/08/1988 || 10 || || 10"
+    )
+  })
+
 })
