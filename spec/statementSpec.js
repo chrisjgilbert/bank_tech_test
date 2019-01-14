3fe7a5d8a829947eb36c810a2acc4d237ef9d6c8
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 20:37:02 +0000

    removed statement showHeader() as not needed

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -7,10 +7,6 @@ describe("printer", function() {
     statement = new Statement()
   })
 
-  it("stores the statement header", function() {
-    expect(statement.showHeader()).toEqual(["DATE", "DEBIT", "CREDIT", "BALANCE"])
-  })
-
   it("prints out the statement", function() {
     var history = [["01/01/1998", 100, "", 100]]
     spyOn(window.console, 'log');
--- src/statement.js
+++ src/statement.js
@@ -10,17 +10,13 @@
   }
 
   Statement.prototype = {
-    showHeader: function() {
-      return this._header;
-    },
-
     print: function(history) {
       console.log(this._header.join(" || "))
       history.forEach(function(transaction) {
         console.log(transaction.join(" || "))
       })
     }
-  }
+  } 
 
   exports.Statement = Statement
 })(this)
