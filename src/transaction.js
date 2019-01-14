80cbf701683f25b6d32639069fe2afb46b60113d
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 13:58:50 +0000

    green: transaction has a type

--- src/transaction.js
+++ src/transaction.js
@@ -1,1 +1,14 @@
-function Transaction() {}
+(function(exports) {
+
+  function Transaction(type) {
+    this._type = type
+  }
+
+  Transaction.prototype = {
+    showType: function() {
+      return this._type
+    }
+  }
+
+  exports.Transaction = Transaction
+})(this)
