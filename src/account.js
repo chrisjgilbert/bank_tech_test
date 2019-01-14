925a571b98bc1292c22f5e4f369bc65ff0e111b4
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 19:04:18 +0000

    green: deposit adds date to transaction history

--- src/account.js
+++ src/account.js
@@ -1,15 +1,20 @@
 "use strict";
 (function(exports) {
 
-  function Account(history = new History) {
+  function Account(history = new History, transaction = new Transaction) {
     this._balance = 0
     this.history = history
+    this._transaction = transaction
   }
 
   Account.prototype = {
     deposit: function(amount) {
       this._balance += amount
-      this.history.add()
+      this.history.add(
+        [
+          this._transaction.showDate()
+        ]
+      )
     },
 
     withdraw: function(amount) {
