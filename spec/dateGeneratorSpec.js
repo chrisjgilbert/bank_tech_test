69b0e69fdf6b800c7f61f41a3ca74e9640ec2658
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 14:15:55 +0000

    red: updated test

--- spec/dateGeneratorSpec.js
+++ spec/dateGeneratorSpec.js
@@ -2,6 +2,10 @@ describe("dateGenerator", function() {
 
   var dateGenerator;
 
+  beforeEach(function() {
+    dateGenerator = new DateGenerator
+  })
+
   it("returns the current day", function() {
     expect(dateGenerator.getDay()).toEqual(14)
   })
