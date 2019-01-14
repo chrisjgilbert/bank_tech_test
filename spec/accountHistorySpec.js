a766730964ba39599ba879265173dd31f547bd6f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 13:19:17 +0000

    red: account history can be instantiated

---
+++ spec/accountHistorySpec.js
@@ -0,0 +1,13 @@
+describe("accountHistory", function() {
+
+  var accountHistory;
+
+  beforeEach(function() {
+    accountHistory = new AccountHistory()
+  })
+
+  it("can be instantiated", function() {
+    expect(accountHistory).toBeTypeOf(AccountHistory)
+  })
+
+})
