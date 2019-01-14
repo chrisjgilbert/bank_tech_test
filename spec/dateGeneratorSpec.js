aea70f30a8d33d43d80942a5225f184de5b550ac
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 15:07:39 +0000

    jasmine spyobj created and injects into dategen trsts

--- spec/dateGeneratorSpec.js
+++ spec/dateGeneratorSpec.js
@@ -3,29 +3,29 @@ describe("dateGenerator", function() {
 
   var dateGenerator;
 
+  var date = jasmine.createSpyObj('date', {
+    'getDate': 14,
+    'getMonth': 0,
+    'getFullYear': 2019
+  });
+
   beforeEach(function() {
-    dateGenerator = new DateGenerator
+    dateGenerator = new DateGenerator(date)
   })
 
   it("returns the current day", function() {
-    spyOn(dateGenerator._date, "getDate").and.returnValue(14);
     expect(dateGenerator._getDay()).toEqual(14)
   })
 
   it("returns the current month + 1", function() {
-    spyOn(dateGenerator._date, "getMonth").and.returnValue(1);
-    expect(dateGenerator._getMonth()).toEqual(2)
+    expect(dateGenerator._getMonth()).toEqual(1)
   })
 
   it("returns the current year", function() {
-    spyOn(dateGenerator._date, "getFullYear").and.returnValue(2019);
     expect(dateGenerator._getYear()).toEqual(2019)
   })
 
   it("returns full date", function() {
-    spyOn(dateGenerator._date, "getDate").and.returnValue(14);
-    spyOn(dateGenerator._date, "getMonth").and.returnValue(0);
-    spyOn(dateGenerator._date, "getFullYear").and.returnValue(2019);
     expect(dateGenerator.getFullDate()).toEqual("14/01/2019")
   })
 
