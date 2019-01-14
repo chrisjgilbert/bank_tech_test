612a9a2480320d7de1c524b2e9aeabeec5d755b0
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 15:13:26 +0000

    spy objects now fully tests in dateGenerator.js

--- spec/dateGeneratorSpec.js
+++ spec/dateGeneratorSpec.js
@@ -13,20 +13,43 @@ describe("dateGenerator", function() {
     dateGenerator = new DateGenerator(date)
   })
 
-  it("returns the current day", function() {
-    expect(dateGenerator._getDay()).toEqual(14)
+  describe("_getDay", function() {
+    it("calls on Date to return the day as an integer", function() {
+      dateGenerator._getDay()
+      expect(date.getDate).toHaveBeenCalled()
+    })
+
+    it("returns the current day", function() {
+      expect(dateGenerator._getDay()).toEqual(14)
+    })
   })
 
-  it("returns the current month + 1", function() {
-    expect(dateGenerator._getMonth()).toEqual(1)
+  describe("_getMonth", function() {
+    it("calls on Date to return the year as an integer", function() {
+      dateGenerator._getMonth()
+      expect(date.getMonth).toHaveBeenCalled()
+    })
+
+    it("returns the current month + 1", function() {
+      expect(dateGenerator._getMonth()).toEqual(1)
+    })
   })
 
-  it("returns the current year", function() {
-    expect(dateGenerator._getYear()).toEqual(2019)
+  describe("_getYear", function() {
+    it("calls on Date to return the year as an integer", function() {
+      dateGenerator._getYear()
+      expect(date.getFullYear).toHaveBeenCalled()
+    })
+
+    it("returns the current year", function() {
+      expect(dateGenerator._getYear()).toEqual(2019)
+    })
   })
 
-  it("returns full date", function() {
-    expect(dateGenerator.getFullDate()).toEqual("14/01/2019")
+  describe("getFullDate", function() {
+    it("returns full date", function() {
+      expect(dateGenerator.getFullDate()).toEqual("14/01/2019")
+    })
   })
 
 })
