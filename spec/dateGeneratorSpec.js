9f06ad3090ded64d18cf85c62e92889bf01352c5
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 15:00:45 +0000

    refactored all dateGenerator tests to now include stubbed time

--- spec/dateGeneratorSpec.js
+++ spec/dateGeneratorSpec.js
@@ -1,3 +1,4 @@
+"use strict";
 describe("dateGenerator", function() {
 
   var dateGenerator;
@@ -7,18 +8,24 @@ describe("dateGenerator", function() {
   })
 
   it("returns the current day", function() {
+    spyOn(dateGenerator._date, "getDate").and.returnValue(14);
     expect(dateGenerator._getDay()).toEqual(14)
   })
 
-  it("returns the current month", function() {
-    expect(dateGenerator._getMonth()).toEqual(1)
+  it("returns the current month + 1", function() {
+    spyOn(dateGenerator._date, "getMonth").and.returnValue(1);
+    expect(dateGenerator._getMonth()).toEqual(2)
   })
 
   it("returns the current year", function() {
+    spyOn(dateGenerator._date, "getFullYear").and.returnValue(2019);
     expect(dateGenerator._getYear()).toEqual(2019)
   })
 
   it("returns full date", function() {
+    spyOn(dateGenerator._date, "getDate").and.returnValue(14);
+    spyOn(dateGenerator._date, "getMonth").and.returnValue(0);
+    spyOn(dateGenerator._date, "getFullYear").and.returnValue(2019);
     expect(dateGenerator.getFullDate()).toEqual("14/01/2019")
   })
 
